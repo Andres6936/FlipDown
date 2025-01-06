5a9dcf0f8ca5926aade67230d7bd97662c52471f
Andres6936 <andres6936@gmail.com>
2025-01-06 15:11:49 -0500

    Refactor Rotor component to use styled-components.
    Replaced CSS file styles with styled-components in Rotor.tsx for better encapsulation and maintainability. Removed redundant CSS rules and updated package dependencies to include styled-components. This improves code readability and supports modern styling practices.

--- packages/react-flipdown/src/Rotor.tsx
+++ packages/react-flipdown/src/Rotor.tsx
@@ -1,6 +1,101 @@
 import {useEffect, useRef, useState} from "react";
 
 import './Rotor.css'
+import styled from "styled-components";
+
+const RotorContainer = styled.div`
+    position: relative;
+    float: left;
+    width: 50px;
+    height: 80px;
+    margin: 0 5px 0 0;
+    border-radius: 4px;
+    font-size: 4rem;
+    text-align: center;
+    perspective: 200px;
+    
+    :last-child {
+        margin-right: 0;
+    }
+    
+    :after {
+        content: '';
+        z-index: 2;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 50px;
+        height: 40px;
+        border-radius: 0 0 4px 4px;
+    }
+
+    @media (prefers-color-scheme: dark) {
+        color: #FFFFFF;
+        background-color: #202020;
+
+        :after {
+            border-top: solid 1px #151515;
+        }
+    }
+
+    @media (prefers-color-scheme: light) {
+        color: #222222;
+        background-color: #DDDDDD;
+        
+        :after {
+            border-top: solid 1px #222222;
+        }
+    }
+`
+
+const RotorTop = styled.div`
+    overflow: hidden;
+    position: absolute;
+    width: 50px;
+    height: 40px;
+    
+    line-height: 80px;
+    border-radius: 4px 4px 0 0;
+
+    @media (prefers-color-scheme: dark) {
+        color: #FFFFFF;
+        background-color: #202020;
+    }
+
+    @media (prefers-color-scheme: light) {
+        color: #222222;
+        background-color: #DDDDDD;
+    }
+`
+
+const RotorBottom = styled.div`
+    overflow: hidden;
+    position: absolute;
+    width: 50px;
+    height: 40px;
+    
+    bottom: 0;
+    line-height: 0;
+    border-radius: 0 0 4px 4px;
+
+    @media (prefers-color-scheme: dark) {
+        color: #EFEFEF;
+        background-color: #202020;
+    }
+
+    @media (prefers-color-scheme: light) {
+        color: #333333;
+        background-color: #EEEEEE;
+    }
+`
+
+const RotorLeaf = styled.div`
+    z-index: 1;
+    position: absolute;
+    width: 50px;
+    height: 80px;
+    transform-style: preserve-3d;
+`
 
 type Props = {
     value: string;
@@ -21,8 +116,8 @@ export function Rotor({value, prevValue}: Props) {
     }, [value]);
 
     return (
-        <div className="rotor">
-            <div ref={rotorLeafRef} className="rotor-leaf flipped">
+        <RotorContainer className="rotor">
+            <RotorLeaf ref={rotorLeafRef} className="rotor-leaf flipped">
                 <figure className="rotor-leaf-rear">
                     {value}
                 </figure>
@@ -30,15 +125,15 @@ export function Rotor({value, prevValue}: Props) {
                 <figure className="rotor-leaf-front">
                     {prevValue}
                 </figure>
-            </div>
+            </RotorLeaf>
 
-            <div className="rotor-top">
+            <RotorTop className="rotor-top">
                 {value}
-            </div>
+            </RotorTop>
 
-            <div className="rotor-bottom">
+            <RotorBottom className="rotor-bottom">
                 {prevValue}
-            </div>
-        </div>
+            </RotorBottom>
+        </RotorContainer>
     )
 }
